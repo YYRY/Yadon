7d522902d25b0ed0427aad3c2f45fa3a94930941
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-11-11 12:21:00 +0900

    header,footer削除
    include文挿入

---
+++ list.php
@@ -0,0 +1,80 @@
+<?php
+	$host_name = "localhost";
+	$dbms_user = "root";
+	$dbms_pass = "";
+
+	//DBMS(MySQL)へ接続
+	$con = mysql_connect($host_name,$dbms_user,$dbms_pass);
+	mysql_select_db("iw32",$con);
+
+	//sql文作成
+	$sql = "SELECT title ,description ,s.movie_id ,s.movie_start ,m.3d FROM movie_m AS m JOIN schedule AS s ON m.movie_id = s.movie_id ORDER BY movie_start DESC";
+
+	//sql実行
+	$res = mysql_query($sql , $con);
+
+
+?>
+<?php
+include('../header.php');
+?>
+	<div id="main">
+		<div id="leftcolumn">
+<?php
+
+			while($row = mysql_fetch_array($res)){
+				if( $row["3d"] ==0){
+					$real = "なし";
+				}else{
+					$real = "あり";
+				}
+				echo "<div class=\"box1\">";
+					echo"<div class=\"box1_img\">";
+						echo "<img src='../img/moviePhoto/".$row['movie_id'].".jpg'\" width=\"150px\" height=\"100px\">";
+					echo "</div>";
+					echo "<div class=\"box1_text\">";
+						echo $row["title"];
+					echo "</div>";
+					echo "<div class=\"box1_text\">";
+						echo $row["description"];
+					echo "</div>";
+					echo "<div class=\"clear\"></div>";
+					echo "<div class=\"box2_text\">";
+						 echo "<div class=\"movieStart\">".$row["movie_start"]."～</div>";
+						 echo "3D上映".$real;
+					echo "</div>";
+				echo "</div>";
+			}
+?>
+		</div><!-- leftcolumn fin-->
+
+		<div id="rightcolumn">
+			<table class="company">
+    				<tbody>
+					<tr>
+				            <th class="arrow_box">お知らせ</div></th>
+				        </tr>
+
+				        <tr>
+				            <th class="arrow_box2">2014-08-00</div></th>
+				            <td>-------------</td>
+				        </tr>
+
+				        <tr>
+				            <th class="arrow_box">2014-08-00</th>
+				            <td>-------------</td>
+				        </tr>
+					
+					<tr>
+				            <th class="arrow_box"></th>
+				            <td></td>
+				        </tr>
+    				</tbody>
+			</table>
+		</div><!-- rightcolumn fin-->
+		<div class="clear"></div>
+
+	</div><!-- main fin -->
+<?php
+include('../footer.php');
+?>
