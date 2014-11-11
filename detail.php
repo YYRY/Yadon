46411154d05dd5f1e617e43a3d4a40858e837b94
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-11-11 10:25:10 +0900

    headerとfooterを削除してinclude文に

---
+++ detail.php
@@ -0,0 +1,50 @@
+<?php
+include('../header.php');
+?>
+	<div id="main">
+		<div id="main_img">
+			<img src="../../../映画情報/1.jpg" width="300px" height="150px">
+		</div>
+
+		<div id="main_text">
+		ノア　約束の舟<br />
+		<p>
+			ある夜、ノアは眠りの中で、恐るべき光景を見る。それは、堕落した人間を滅ぼすために、すべてを地上から消し去り、新たな世界を創るという神の宣告だった。大洪水が来ると知ったノアは、家族と共に、罪のない動物たちを守る箱舟を作り始める。やがて大洪水が始まる。空は暗転し激しい豪雨が大地に降り注ぎ、地上の水門が開き水柱が立ち上がる。濁流が地上を覆う中、ノアの家族と動物たちを乗せた箱舟だけが流されていく。閉ざされた箱舟の中で、ノアは神に託された驚くべき使命を打ち明ける。箱舟に乗ったノアの家族の未来とは？ 人類が犯した罪とは？そして世界を新たに創造するという途方もない約束の結末とは──？
+		</p>
+		</div>
+	<div class="clear"></div>
+	</div><!-- main fin -->
+	<div class="clear"></div>
+
+	<div class="full_content">
+   		<div class="menu active">9/1(月)</div>
+ 	  		<div class="content">
+   			<div class="drag"><img src="../img/movie/men.png"></div>
+			<div class="drag"><img src="../img/movie/girl.png"></div>
+			<div class="drag"><img src="../img/movie/kuruma.png"></div>  
+   			</div>
+       
+   		<div class="menu">9/2(火)</div>
+   			<div class="content">
+   			ここに内容が入ります。    
+   		</div>
+ 
+		<div class="menu">9/3(水)</div>
+		   	<div class="content">
+		   	ここに内容が入ります。    
+		</div>
+   
+		<div class="menu">9/4(木)</div>
+		   	<div class="content">
+		   	ここに内容が入ります。    
+		</div>
+    
+   		<div class="menu">9/5(金)</div>
+   			<div class="content">
+   			ここに内容が入ります。    
+   			</div>
+		</div>
+
+<?php
+include('../footer.php');
+?>
