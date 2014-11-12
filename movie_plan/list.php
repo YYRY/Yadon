0fce1ace031be953e6464eabb61fe5722f04d31e
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-11-12 14:59:21 +0900

    movie_plan/detail php使用

--- movie_plan/list.php
+++ movie_plan/list.php
@@ -2,21 +2,25 @@
 	$host_name = "localhost";
 	$dbms_user = "root";
 	$dbms_pass = "";
+
+	//DBMS(MySQL)へ接続
 	$con = mysql_connect($host_name,$dbms_user,$dbms_pass);
-
 	mysql_select_db("iw32",$con);
-	$sql = "SELECT title, description ,s.movie_id ,s.movie_start ,m.3d FROM movie_m AS m JOIN schedule AS s ON m.movie_id = s.movie_id ORDER BY movie_start DESC";
 
+	//sql文作成
+	$sql = "SELECT title ,description ,s.movie_id ,s.movie_start ,m.3d FROM movie_m AS m JOIN schedule AS s ON m.movie_id = s.movie_id ORDER BY movie_start DESC";
+
+	//sql実行
 	$res = mysql_query($sql , $con);
-	mysql_close($con);
-	
+
+
 ?>
 <?php
 include('../header.php');
 ?>
 	<div id="main">
-		<div id="leftcolumn">
 <?php
+
 			while($row = mysql_fetch_array($res)){
 				if( $row["3d"] ==0){
 					$real = "なし";
@@ -40,28 +44,8 @@ include('../header.php');
 					echo "</div>";
 				echo "</div>";
 			}
-			$hakoDay = array('日曜めっちゃお得デイ♪','menzu','シニア割','ハードゲイ','レディースデイ','華の金曜！','うんこ');
-			$hakoMoney = array(3300,2500,300,500,400,300,300); 
-			$day = date('w');
 ?>
-		</div>
-	<!-- leftcolumn fin-->
-
-		<div id="rightcolumn">
-			<table class="company">
-    				<tbody>
-						<tr>
-				        	<th class="arrow_box">今日のサービス</div></th>
-				        </tr>
-				        <tr>
-				            <th class="arrow_box"><?php echo $hakoDay[$day];?></th>
-				            <td><?php echo "チケットから".$hakoMoney[$day]."円引きです。";?></td>
-				        </tr>
-    				</tbody>
-			</table>
-		</div><!-- rightcolumn fin-->
-		<div class="clear"></div>
-
+		</div><!-- leftcolumn fin-->
 	</div><!-- main fin -->
 <?php
 include('../footer.php');
