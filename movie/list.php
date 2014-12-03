ed2810e36cb29b75315f410edc697a41f52c636b
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-12-03 15:07:51 +0900

    movie/list　レイアウト更新
    レイアウト更新

--- movie/list.php
+++ movie/list.php
@@ -15,9 +15,11 @@
 ?>
 <?php
 include('../header.php');
-include "../include_session/session.php";
 ?>
+	<link href="../css/index.css" rel="stylesheet" type="text/css">
+	<link href="../css/movie/movie.css" rel="stylesheet" type="text/css">
 	<div id="main">
+		<div id="leftcolumn">
 <?php
 
 			while($row = mysql_fetch_array($res)){
@@ -27,24 +29,74 @@ include "../include_session/session.php";
 					$real = "あり";
 				}
 				echo "<div class=\"box1\">";
+					echo "<table>";
+						echo "<tr>";
+						echo "<td style=\"width:5px; height: 30px; background-color:#0000ff;\"></td>";
+						echo "<td><strong>".$row["title"]."</strong></td>";
+						echo "</tr>";
+					echo "</table>";
 					echo"<div class=\"box1_img\">";
-						echo "<img src='../img/moviePhoto/".$row['movie_id'].".jpg'\" width=\"150px\" height=\"100px\">";
-					echo "</div>";
-					echo "<div class=\"box1_text\">";
-						echo $row["title"];
+						echo "<img src='../img/moviePhoto/".$row['movie_id'].".jpg'\" width=\"200px\" height=\"auto\">";
 					echo "</div>";
 					echo "<div class=\"box1_text\">";
 						echo $row["description"];
 					echo "</div>";
 					echo "<div class=\"clear\"></div>";
 					echo "<div class=\"box2_text\">";
-						 echo "<div class=\"movieStart\">".$row["movie_start"]."～</div>";
-						 echo "3D上映".$real;
+						echo "<div class=\"movieStart\">".$row["movie_start"]."～</div>";
+						echo "3D上映".$real;
 					echo "</div>";
+					echo "<a class=\"btn\" href=\"#\">作品詳細</a>";
+					echo "<div class=\"clear\"></div>";
+					echo "<hr size=\"3\" color=\"black\" width=\"100%\">";
+					
+					
 				echo "</div>";
 			}
 ?>
 		</div><!-- leftcolumn fin-->
+		<div id="rightcolumn">
+			<div class="right_waku">
+			<div class="right_h3">お知らせ</div>
+			<div class="new">
+				<ul>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				</ul>
+			</div>
+			</div>
+
+			<div class="right_waku">
+			<div class="right_h3">お知らせ</div>
+			<div class="new">
+				<ul>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				<li>00/00：更新しました</li>
+				</ul>
+			</div>
+			</div>
+							
+			</div>
+			<img src="../img/movie/koukousei_r.jpg" width="295px">
+			<img src="../img/movie/koukousei_r.jpg" width="295px">
+			<img src="../img/movie/koukousei_r.jpg" width="295px">
+			<img src="../img/movie/koukousei_r.jpg" width="295px">
+			</div>
+		
+		</div><!-- rightcolumn fin-->
+		<div class="clear"></div>
 	</div><!-- main fin -->
 <?php
 include('../footer.php');
