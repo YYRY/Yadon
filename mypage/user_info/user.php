67aef06019863a4dc9b5d43205f232095c1452a2
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-12-09 11:50:36 +0900

    user.php フッターtmpl表示
    includeしてあります

--- mypage/user_info/user.php
+++ mypage/user_info/user.php
@@ -68,34 +68,6 @@ $res = mysql_query($user_info , $con);
 	<!-- main table fin -->
 	<a href="user_insert.php">情報変更</a>
 	<a href="">よく買う人登録</a>
-			<!-- フッター -->
-		<footer>
-			<section>
-				<h3>分類１</h3>
-				<ul>
-					<li><a href="">プライバシーポリシー</a></li>
-					<li><a href="">プライバシーポリシー</a></li>
-					<li><a href="">プライバシーポリシー</a></li>
-				</ul>
-			</section>
-			<section>
-				<h3>分類２</h3>
-				<ul>
-					<li><a href="">特定商取引法に基づく表記</a></li>
-					<li><a href="">特定商取引法に基づく表記</a></li>
-					<li><a href="">特定商取引法に基づく表記</a></li>
-				</ul>
-			</section>
-			<section>
-				<h3>分類３</h3>
-				<ul>
-					<li><a href="">ご利用に際して</a></li>
-					<li><a href="">ご利用に際して</a></li>
-					<li><a href="">ご利用に際して</a></li>
-				</ul>
-			</section>
-
-			<p><small>&copy; 2014 HAL Cinema Ltd. All Rights Reserved.</small></p>
-		</footer>
-</body>
-</html>
+<?php
+include ('../../footer.php');
+?>
