09ea40c0f4907db538568a8fbd9eb8b0017c9172
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-12-17 14:50:47 +0900

    マイページ項目削除
    メルマガ項目削除

--- mypage/mypage.php
+++ mypage/mypage.php
@@ -9,7 +9,6 @@ $customer_id = 4;//$_SESSION["c_id"];
 			<div><a href="user_info/user.php?customer_id=<?php echo $customer_id?>">ユーザ情報</a></div>
 			<div><a href="history_info/history.php">購入履歴</a></div>
 			<div><a href="cregit_info/cregit.php">クレジットカード情報</a></div>
-			<div><a href="mail_info/mail.html">メルマガ登録・変更</a></div>
 			<div><a href="user_deleate.php?customer_id=<?php echo $customer_id?>">退会する</a></div>
 		</section>
 		<section class="right-cont">
