a093a656e4058f89c0fb5d42ec6afdad244c148e
設楽 開人 <k_kaito_1004@yahoo.co.jp>
2014-12-17 15:19:52 +0900

    マイページ　大会機能更新
    jsの追加

--- mypage/mypage.php
+++ mypage/mypage.php
@@ -9,8 +9,15 @@ $customer_id = 4;//$_SESSION["c_id"];
 			<div><a href="user_info/user.php?customer_id=<?php echo $customer_id?>">ユーザ情報</a></div>
 			<div><a href="history_info/history.php">購入履歴</a></div>
 			<div><a href="cregit_info/cregit.php">クレジットカード情報</a></div>
-			<div><a href="user_deleate.php?customer_id=<?php echo $customer_id?>">退会する</a></div>
+			<div onclick="del('${customer_id}');"><a href="user_deleate.php?customer_id=<?php echo $customer_id?>">退会する</a></div>
 		</section>
+<script type="text/javascript">
+function del(n){
+	if(confirm("本当に退会しますか？")){
+		location.href = "user_deleate.php?customer_id="+n;
+	}
+}
+</script>
 		<section class="right-cont">
 			<div>有用情報</div>
 		</section>
